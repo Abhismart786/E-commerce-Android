c1ba384e6b50ac8a9fe48134e80e5132df5caa05
Abhismart786 <abhiKhatri097@gmail.com>
2025-03-03 10:11:02 -0500

    7 commit

--- app/src/main/java/com/example/e_commerce_app/ForgetPassword.java
+++ app/src/main/java/com/example/e_commerce_app/ForgetPassword.java
@@ -8,17 +8,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 public class ForgetPassword extends AppCompatActivity {
+
+    private Button loginRedirectButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_forget_password);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        loginRedirectButton = findViewById(R.id.loginRedirectButton);
+
+        // Redirect to LoginActivity when the "Back to Login" button is clicked
+        loginRedirectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Redirect to Login Activity
+                Intent intent = new Intent(ForgetPassword.this, LoginActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
